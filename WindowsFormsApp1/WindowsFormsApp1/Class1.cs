be03e8b5118c4a0f7e1272dd2381e91e909a5a51
ducthanh <22110416@student.hcmute.edu.vn>
2024-01-26 16:02:41 +0700

    Update class

---
+++ WindowsFormsApp1/WindowsFormsApp1/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    internal class Class1
+    {
+        int x;
+        int y;
+    }
+}
